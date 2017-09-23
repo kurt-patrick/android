1e4b6a5cf518e5dd87216cd901f1bca7db124703
kp <kurt_patrick_online@hotmail.com>
2017-09-23 20:55:18 +0800

    updated gradle for recyclerview

--- Books/app/src/main/java/com/android/kpe/books/BooksAdapter.java
+++ Books/app/src/main/java/com/android/kpe/books/BooksAdapter.java
@@ -1,14 +1,95 @@
 package com.android.kpe.books;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
 /**
  * Created by LocalUser on 23/09/2017.
  */
 
-//import android.support.v7.widget.Rec
+public class BooksAdapter extends RecyclerView.Adapter<BooksAdapter.BookViewHolder> {
 
-public class BooksAdapter {
+    ArrayList<Book> mBooks = null;
+    public BooksAdapter(ArrayList<Book> books) {
+        this.mBooks = books;
+    }
+
+    @Override
+    public BookViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        View itemView = LayoutInflater.from(context).inflate(R.layout.book_list_item, parent, false);
+        return new BookViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(BookViewHolder holder, int position) {
+        Book book = mBooks.get(position);
+        holder.bind(book);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mBooks.size();
+    }
 
     public class BookViewHolder extends RecyclerView.ViewHolder {
 
+        TextView mAuthors = null;
+        TextView mPublishedDate = null;
+        TextView mPublisher = null;
+        TextView mTitle = null;
+
+        public BookViewHolder(View itemView) {
+            super(itemView);
+            mAuthors = (TextView) itemView.findViewById(R.id.txtAuthors);
+            mPublishedDate = (TextView) itemView.findViewById(R.id.txtPublishedDate);
+            mPublisher = (TextView) itemView.findViewById(R.id.txtPublisher);
+            mTitle = (TextView) itemView.findViewById(R.id.txtTitle);
+        }
+
+        public void bind (Book book) {
+            mTitle.setText(book.title);
+            mPublishedDate.setText(book.publishedDate);
+            mPublisher.setText(book.publisher);
+
+            StringBuilder sb = new StringBuilder();
+            for(int index=0; index < book.authors.length; index++) {
+                sb.append(book.authors[index]);
+                if(index < book.authors.length -1) {
+                    sb.append(", ");
+                }
+            }
+            mAuthors.setText(sb.toString());
+
+        }
+
+        /*
+        TextView getTitle() {
+            if(mTitle == null) {
+                mTitle = (TextView)
+            }
+            return mTitle;
+        }
+
+        TextView getAuthors() {
+
+        }
+
+        TextView getDate() {
+
+        }
+
+        TextView getPublisher() {
+
+        }
+        */
+
     }
+
 }
